bd59eb7679fdbcdcc1f4dc4a4ac5b14f08560146
Irfan Ansari <exitir@gmail.com>
2018-07-24 04:15:36 +0530

    added sample link

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -7,6 +7,9 @@ import "./ImageLinkForm.css";
         <p className="f3">
           {"This Magic brain will detect faces in your pictures, Give it a try!"}
         </p>
+        <div className="f4">
+          {"e.g. http://im.rediff.com/movies/2015/nov/21south1.jpg"}
+        </div>
         <div className="center">
           <div className="form center pa4 br3 shadow-5">
             <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange} placeholder="Enter Image url"/>
